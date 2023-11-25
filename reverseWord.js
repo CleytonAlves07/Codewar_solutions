fbaf0c6e369add2c5dfe2af7c5ad0dcf2c70c609
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-11-25 11:33:42 -0300

    feat: :sparkles: reverse word solution

---
+++ reverseWord.js
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+STRINGSFUNDAMENTALS
+*/
+
+
+function reverseWords(str) {
+    arrayReverse = str.split(' ')
+    for(i in arrayReverse) {
+        arrayReverse[i] = arrayReverse[i].split('').reverse().join('')
+    }
+    return console.log(arrayReverse.join(' '));
+}
+
+reverseWords('The quick brown fox jumps over the lazy dog.')
